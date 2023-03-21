0ac655d878aefb5d7661ada66b70e141564e7630
jsots <justinsotolongo@gmail.com>
2023-03-21 11:57:53 -0400

    Some features added to make it look nicer. The index number, the buttons glow when you hover

--- script.js
+++ script.js
@@ -13,9 +13,11 @@ fetch(baseURL)
         let digiImg = document.querySelector(".digi-img")
         let digiName = document.querySelector(".digi-name")
         let digiLevel = document.querySelector(".digi-level")
+        let digiNo = document.querySelector("h3")
         digiImg.src = `${res[0].img}`
         digiName.innerText = `${res[0].name}`
         digiLevel.innerText = `${res[0].level}`
+        digiNo.innerText = "00" + `${prevIndex}`
     })
 
 
@@ -26,16 +28,25 @@ nextB.addEventListener("click", () => {
             let digiImg = document.querySelector(".digi-img")
             let digiName = document.querySelector(".digi-name")
             let digiLevel = document.querySelector(".digi-level")
+            let digiNo = document.querySelector("h3")
             if (prevIndex === res.length-1) {
                 digiImg.src = `${res[0].img}`
                 digiName.innerText = `${res[0].name}`
                 digiLevel.innerText = `${res[0].level}`
                 prevIndex = 0
+                digiNo.innerText = "00" + `${prevIndex}`
             } else {
                 digiImg.src = `${res[prevIndex+1].img}`
                 digiName.innerText = `${res[prevIndex+1].name}`
                 digiLevel.innerText = `${res[prevIndex+1].level}`
                 prevIndex += 1
+                if (prevIndex < 10) {
+                    digiNo.innerText = "00" + `${prevIndex}`
+                } else if (prevIndex < 100) {
+                    digiNo.innerText = "0" + `${prevIndex}`
+                } else {
+                    digiNo.innerText = `${prevIndex}`
+                }
             }
         })
 })
@@ -47,16 +58,26 @@ prevB.addEventListener("click", () => {
             let digiImg = document.querySelector(".digi-img")
             let digiName = document.querySelector(".digi-name")
             let digiLevel = document.querySelector(".digi-level")
+            let digiNo = document.querySelector("h3")
             if (prevIndex === 0) {
                 digiImg.src = `${res[res.length-1].img}`
                 digiName.innerText = `${res[res.length-1].name}`
                 digiLevel.innerText = `${res[res.length-1].level}`
+                digiNo.innerText = `${prevIndex}`
                 prevIndex = res.length-1
+                digiNo.innerText = `${prevIndex}`
             } else {
                 digiImg.src = `${res[prevIndex-1].img}`
                 digiName.innerText = `${res[prevIndex-1].name}`
                 digiLevel.innerText = `${res[prevIndex-1].level}`
                 prevIndex -= 1
+                if (prevIndex < 10) {
+                    digiNo.innerText = "00" + `${prevIndex}`
+                } else if (prevIndex < 100) {
+                    digiNo.innerText = "0" + `${prevIndex}`
+                } else {
+                    digiNo.innerText = `${prevIndex}`
+                }
             }
         })
 })
@@ -69,9 +90,17 @@ randomB.addEventListener("click", () => {
             let digiName = document.querySelector(".digi-name")
             let digiLevel = document.querySelector(".digi-level")
             let rng = Math.floor(Math.random()*res.length+1)
+            let digiNo = document.querySelector("h3")
             digiImg.src = `${res[rng].img}`
             digiName.innerText = `${res[rng].name}`
             digiLevel.innerText = `${res[rng].level}`
             prevIndex = rng
+            if (prevIndex < 10) {
+                digiNo.innerText = "00" + `${prevIndex}`
+            } else if (prevIndex < 100) {
+                digiNo.innerText = "0" + `${prevIndex}`
+            } else {
+                digiNo.innerText = `${prevIndex}`
+            }
         })
 })
